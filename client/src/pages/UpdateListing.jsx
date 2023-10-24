e16e52ac35ff5008efcb0805d01f9fd4231ee6ae
Aman-rex <15singh.aman@gmail.com>
2023-10-24 22:43:44 +0530

    Completed listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -303,7 +303,8 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs">($/month)</span>
+                {formData.type==='rent' && (
+                <span className="text-xs">($/month)</span>)}
               </div>
             </div>
             {formData.offer && (
@@ -320,7 +321,8 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">($/month)</span>
+                  {formData.type==='rent' && (
+                  <span className="text-xs">($/month)</span>)}
                 </div>
               </div>
             )}
