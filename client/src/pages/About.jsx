d24e86f0f950e9296be352af8a53e91cbccecf9c
Aman-rex <15singh.aman@gmail.com>
2023-10-28 01:26:27 +0530

    Completed about Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-400'>About Aman Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ex, exercitationem neque, odio molestias numquam velit voluptate esse incidunt, aliquid deserunt maxime illo asperiores nam illum nostrum ipsa magni commodi.</p>    
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, incidunt suscipit. Vel, alias at tempora nam a, sed ipsa maxime obcaecati odit natus beatae praesentium corrupti quasi vitae atque? Quae.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas dolorum cumque ex blanditiis deserunt, quae quidem vel omnis expedita et quas dolore molestias consectetur placeat beatae, incidunt debitis numquam deleniti?</p>
+    </div>
   )
 }
